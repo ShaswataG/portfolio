543d5bb98cedb326519452ddcac8761a34c1a803
ShaswataG <121812904+ShaswataG@users.noreply.github.com>
2023-08-21 20:46:54 +0530

    javascript animation added

---
+++ behaviour.js
@@ -0,0 +1,35 @@
+$(".section-nav").hover(
+    function () {
+        $(this).addClass("nav-animate");
+    },
+    function() {
+        $(this).removeClass("nav-animate");
+    }
+);
+
+$(".card").hover(
+    function () {
+        $(this).addClass("project-animate");
+    },
+    function() {
+        $(this).removeClass("project-animate");
+    }
+);
+
+$(".card").hover(
+    function () {
+        $(this).addClass("project-animate");
+    },
+    function() {
+        $(this).removeClass("project-animate");
+    }
+);
+
+$(".project-url").hover(
+    function () {
+        $(this).addClass("project-url-animate");
+    },
+    function() {
+        $(this).removeClass("project-url-animate");
+    }
+);
