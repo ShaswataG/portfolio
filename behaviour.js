d13ec956d941d6e8823c6db86777c5c78e73dc29
ShaswataG <121812904+ShaswataG@users.noreply.github.com>
2023-11-21 18:39:07 +0530

    test

--- behaviour.js
+++ behaviour.js
@@ -33,3 +33,20 @@ $(".project-url").hover(
         $(this).removeClass("project-url-animate");
     }
 );
+
+// $(".theme-toggle").click(function() {
+//     $(".owner-name").toggleClass("owner-name-light");
+//     // $(".content").toggleClass("sidebar");
+//     $(".nav-link").toggle(
+//         function() {
+//             $(".nav-link").css("color", "black");
+//         },
+//         function() {
+//             $(".nav-link").css("color", "white");
+//         }
+        
+//     );
+//     $(".content").toggleClass("content-light-theme");
+//     $("#instagram-profile").attr("src", "./images/instagram-black");
+//     $(".sidebar").toggleClass("sidebar-light-theme");
+// })
